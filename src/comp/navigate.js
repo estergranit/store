88e2bb0ff4843cb2b450434231c8a908e6fefb25
ettyy <ety0504102601@gmail.com>
2022-05-29 22:55:52 +0300

    add search (bug)

--- src/comp/navigate.js
+++ src/comp/navigate.js
@@ -1,14 +1,4 @@
-// import React from 'react'
-
-// export default function Navigate() {
-
-//     return (
-//         <>
-
-
-//         </>
-//     )
-// }
+//from https://mui.com/material-ui/react-app-bar/#app-bar-with-responsive-menu
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -23,17 +13,20 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
-import { useSelector, useDispatch } from 'react-redux'
-// import Login from './login';
-// import SignUp from './signUp';
-import { Link } from 'react-router-dom';
 
+import { useSelector, useDispatch } from 'react-redux'
+import { Link , useNavigate} from 'react-router-dom';
+//search
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
 
 const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const ResponsiveAppBar = () => {
   const data = useSelector(state => state)
+  const navigate = useNavigate()
 
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
@@ -53,36 +46,89 @@ const ResponsiveAppBar = () => {
     setAnchorElUser(null);
   };
   /////for the profile icon------------------------------------------------------------------
+  //from https://mui.com/material-ui/react-avatar/#letter-avatars
   function stringToColor(string) {
     let hash = 0;
     let i;
-  
+
     /* eslint-disable no-bitwise */
     for (i = 0; i < string.length; i += 1) {
       hash = string.charCodeAt(i) + ((hash << 5) - hash);
     }
-  
+
     let color = '#';
-  
+
     for (i = 0; i < 3; i += 1) {
       const value = (hash >> (i * 8)) & 0xff;
       color += `00${value.toString(16)}`.slice(-2);
     }
     /* eslint-enable no-bitwise */
-  
+
     return color;
   }
-  
+
   function stringAvatar(name) {
     return {
       sx: {
         bgcolor: stringToColor(name),
       },
-      children:name[0]
+      children: name[0]
       // children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}` || name[0],
     };
   }
   /////----------------------------------------------------------------------------------------
+  //for search---------------------------------------------------------------------------------
+  const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  }));
+
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('md')]: {
+        width: '20ch',
+      },
+    },
+  }));
+  //miryam****
+  //work but there is an error in the console
+  let productName
+  function searchProductByName() {
+    let searchProduct = data?.product?.products?.find(product => product.name == productName)
+    if (searchProduct && searchProduct.name)
+      console.log("jjjjjjjjjjjjjjjjjjjjjjjjjjj")
+      navigate(`/product-detail/${searchProduct.name}`)
+      return
+  }
+  //-------------------------------------------------------------------------------------------------
+
 
   return (
     <AppBar position="static">
@@ -174,15 +220,28 @@ const ResponsiveAppBar = () => {
               </Button>
             ))}
           </Box>
-
+          {/* search */}
+          <Search>
+            <SearchIconWrapper >
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+              onChange={(e) => {
+                productName = e.target.value
+                searchProductByName()}}
+            />
+          </Search>
+          {/* search----------------- */}
           {data.user.currentUserId > 100 ? <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-              {/* <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" /> */}
-              {console.log(`current ${data.user.currentUserId}`)}
-              {/* {console.log(`find `)} */}
-              {console.log(data.user.users.find(user=>user.userId==data.user.currentUserId).name)}
-              <Avatar {...stringAvatar(data.user.users.find(user=>user.userId==data.user.currentUserId).name)} />
+                {/* <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" /> */}
+                {console.log(`current ${data.user.currentUserId}`)}
+                {/* {console.log(`find `)} */}
+                {console.log(data.user.users.find(user => user.userId == data.user.currentUserId).name)}
+                <Avatar {...stringAvatar(data.user.users.find(user => user.userId == data.user.currentUserId).name)} />
               </IconButton>
             </Tooltip>
             <Menu
--- src/comp/router.js
+++ src/comp/router.js
@@ -14,17 +14,17 @@ import ResponsiveAppBar from './navigate';
 export default function Router() {
     return (
         <BrowserRouter>
-          <ResponsiveAppBar></ResponsiveAppBar>
-        <Routes>
-            <Route path='/' element={<Home/>}></Route>
-            <Route path='/login' element={<Login/>}></Route>
-            <Route path='/signup' element={<SignUp/>}></Route>
-            <Route path='/all-product/:categoryId' element={<AllProduct/>}></Route>
-            <Route path='/product-detail/:productName' element={<ProductDetail/>}></Route>
-            <Route path='/personal-area' element={<PersonalArea/>}></Route>
-            <Route path='/cart' element={<Cart/>}></Route>
-            <Route path='/buy' element={<Buy/>}></Route>
-        </Routes>
+            <ResponsiveAppBar></ResponsiveAppBar>
+            <Routes>
+                <Route path='/' element={<Home />}></Route>
+                <Route path='/login' element={<Login />}></Route>
+                <Route path='/signup' element={<SignUp />}></Route>
+                <Route path='/all-product/:categoryId' element={<AllProduct />}></Route>
+                <Route path='/product-detail/:productName' element={<ProductDetail />}></Route>
+                <Route path='/personal-area' element={<PersonalArea />}></Route>
+                <Route path='/cart' element={<Cart />}></Route>
+                <Route path='/buy' element={<Buy />}></Route>
+            </Routes>
         </BrowserRouter>
     )
 }
